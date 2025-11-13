7910e1a862b3285de15375f4c0a4e7571e386438
Vincent Le Dû <vincent.ledu@gmail.com>
2025-11-13 03:00:01 +0100

    fix gunicorn bug

--- src/webapp/wsgi.py
+++ src/webapp/wsgi.py
@@ -1,4 +1,7 @@
-if __name__ == "__main__":
-    from webapp.app import main
+"""WSGI entrypoint used by gunicorn (`webapp.wsgi:app`)."""
 
+from webapp.app import app as app, main
+
+
+if __name__ == "__main__":
     main()
